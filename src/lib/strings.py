265346886aaf1544bb03aa3d46d79396e55c02b3
harpreet-kaur-mahant <harpreet.kaur.mahant@gmail.com>
2019-12-18 21:00:44 -0700

    adding strings file

--- src/lib/strings.py
+++ src/lib/strings.py
@@ -1,1 +1,25 @@
-print("Strings")
+print("Strings :  Sequence of characters")
+single_quoted_string = 'data science'
+doule_quoted_string = "from scratch"
+mixed_quoted_string = "I am n't going to gym today."
+
+print( single_quoted_string)
+print(doule_quoted_string)
+print(mixed_quoted_string)
+
+print( len(single_quoted_string))
+print( len(doule_quoted_string))
+print( len(mixed_quoted_string))
+
+tab_string = "\t"
+print( len(tab_string) )
+
+raw_tab_string = r"\t"
+print( len(raw_tab_string) )
+
+multi_line_string = """ This is the first line.
+This is second line
+This is third line"""
+
+print(multi_line_string)
+print(len(multi_line_string))
