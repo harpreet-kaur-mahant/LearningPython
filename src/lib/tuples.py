c9faca103d4c67cac81c36dead01e36e5c746f1e
harpreet-kaur-mahant <harpreet.kaur.mahant@gmail.com>
2019-12-21 16:36:33 -0700

    learning tu*les

---
+++ src/lib/tuples.py
@@ -0,0 +1,20 @@
+#learning Tuples/couples
+
+my_tuple = (1,2,3,2)
+print(my_tuple)
+print(type(my_tuple))
+
+#Indexing of the tuple
+
+print( my_tuple[3])
+
+print(len(my_tuple))
+
+#Count the number of occurences of the object in the tuple
+print(my_tuple.count(1))
+print(my_tuple.count(2))
+print(my_tuple.count(3))
+
+#Returns the very first index of the object in the tuple
+print(my_tuple.index(1))
+print(my_tuple.index(2))
